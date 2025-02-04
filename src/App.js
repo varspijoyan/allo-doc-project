d5687d49c61a85e9f9557a64879c100130f6fc11
varspijoyan <varsikpijoyan2003@gmail.com>
2025-02-04 22:56:38 +0400

    Added new  MainPage.jsx file inside pakages folder and added in the App.js file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components/header-component/Header';
 import LanguageSwitchProvider from "../src/contexts/LanguageSwitch";
+import MainPage from './pages/MainPage';
 
 function App() {
   return (
     <LanguageSwitchProvider>
       <Header />
+      <MainPage />
     </LanguageSwitchProvider>
   );
 }
