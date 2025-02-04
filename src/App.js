c9380cbf6baa21971b81c7c4dc254309c0727dcf
varspijoyan <varsikpijoyan2003@gmail.com>
2025-02-04 20:47:13 +0400

    Set up React project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import LanguageSwitchProvider from './contexts/LanguageSwitch';
+
+function App() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
