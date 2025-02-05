9a200a201bae55226766e519846a0958d7ff2d80
varspijoyan <varsikpijoyan2003@gmail.com>
2025-02-05 14:05:14 +0400

    Added border styles for heading and desc in main text container

--- src/components/main-section-components/main-section-text-container-component/MainTextContainerComponent.jsx
+++ src/components/main-section-components/main-section-text-container-component/MainTextContainerComponent.jsx
@@ -8,7 +8,8 @@ export default function MainTextContainerComponent() {
     return (
         <div className="main-section-text-container">
             <h1 className="heading">{translate("Online", language)} {translate("consultations", language)} {translate("from_doctors", language)} {translate("specialists", language)} 24/7</h1>
-            <p className="desc">{translate("Consult", language)} <b>{translate("now", language)}</b> {translate("or", language)} {translate("by", language)} <b>{translate("previous_appointment", language)}</b> {translate("from_your", language)} {translate("computer", language)} {translate("or", language)} {translate("by_using_our_application", language)}, {translate("available_in", language)} <span className="highlight-blue">App Store</span> {translate("and", language)} <span className="highlight-blue">Google Play</span></p>
+            <div className={`h1-border ${language === "ru" ? "ru-h1-border" : "en-h1-border"}`}></div>
+            <p className="desc">{translate("Consult", language)} <b>{translate("now", language)}</b> <span className={`b-1 ${language === "ru" ? "ru-b-1" : "en-b-1"}`}></span> {translate("or", language)} {translate("by", language)} <b>{translate("previous_appointment", language)}</b> <span className={`b-2 ${language === "ru" ? "ru-b-2" : "en-b-2"}`}></span> {translate("from_your", language)} {translate("computer", language)} {translate("or", language)} {translate("by_using_our_application", language)}, {translate("available_in", language)} <span className="highlight-blue">App Store</span> {translate("and", language)} <span className="highlight-blue">Google Play</span></p>
             <div className="buttons">
                 <MakeAnAppointmentButtonComponent />
                 <HowItWorksButtonComponent />
