a0bfd48f10ee284bd7b2c468fcb14e0b802434ff
varspijoyan <varsikpijoyan2003@gmail.com>
2025-02-05 15:36:44 +0400

    Modified appointment section component files and Appointment.css file

--- src/components/appointment-section-component/AppointmentSection.jsx
+++ src/components/appointment-section-component/AppointmentSection.jsx
@@ -1,7 +1,11 @@
+import img from "../../images/OBJECTS.svg";
+import AppointmentTextContainerComponent from "./appointment-text-container-component/AppointmentTextContainerComponent";
+import "../../styles/Appointment.css";
 export default function AppointmentSection() {
     return (
         <section className="appointment-section">
-            
+            <img src={img} alt="Image" /> 
+            <AppointmentTextContainerComponent />        
         </section>
     )
 }
