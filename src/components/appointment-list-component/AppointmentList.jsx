35942b3f892cf54e2e5d8171cc1a8c619140c0fe
varspijoyan <varsikpijoyan2003@gmail.com>
2025-02-08 14:21:10 +0400

    Modified apointmentInfo.json, optimized AppointmentList.jsx and AddedAppointment.jsx components rendering and modified Appointments.css file

--- src/components/appointment-list-component/AppointmentList.jsx
+++ src/components/appointment-list-component/AppointmentList.jsx
@@ -1,19 +1,24 @@
 import "../../styles/AppointmentList.css";
-import { useContext, useState } from "react";
+import { useContext, useState, useMemo } from "react";
 import {LanguageSwitch} from "../../contexts/LanguageSwitch";
 import {translate} from "../../utils/translate";
 import AddedAppointments from "./AddedAppointments";
-
-import calendarImg from "../../images/calendar.svg";
-import clockImg from "../../images/clock.svg";
-import chatImg from "../../images/conversation.svg";
-import videoCameraImg from "../../images/video-camera.svg";
-import docImg from "../../images/doc-img.svg";
 import appointmentInfo from "../../locals/appointmentInfo.json";
 
 export default function AppointmentList() {
     const {language} = useContext(LanguageSwitch);
     const [activeStatusLink, setActiveStatusLink] = useState("status-upcoming");
+    const filteredAppointments = useMemo(() => {
+        return appointmentInfo.filter((data) => {
+            if(activeStatusLink === "status-upcoming")
+                return data.status.planned;
+            if(activeStatusLink === "status-past")
+                return data.status.It_took_place;
+            if(activeStatusLink === "status-cancelled")
+                return data.status.cancelled;
+            return false;
+        });
+    }, [activeStatusLink, appointmentInfo]);
     return (
         <section className="appointment-list">
             <p className={`title ${language === "ru" ? "ru-title" : "en-title"}`}>{translate("My_appointments", language)}</p>
@@ -30,54 +35,8 @@ export default function AppointmentList() {
                         onClick={() => setActiveStatusLink("status-cancelled")}>{translate("Cancelled", language)}</li>
                 </ul>
             </div>
-            {appointmentInfo.map((data) => {
-                if(activeStatusLink === "status-upcoming" && data.status.planned) {
-                    return <AddedAppointments
-                            calendarImage = {calendarImg}
-                            appointmentDate = {data.date}
-                            clockImage = {clockImg}
-                            time = {`${data.time.hours}:${data.time.minutes}:${data.time.seconds}`}
-                            chatImage = {data.connectionType === "Video_connection" ? videoCameraImg : chatImg}
-                            connectionType = {translate(data.connectionType, language)}
-                            status = {translate("Planned", language)}
-                            docImage = {docImg}
-                            fullName = {`${data.firstName} ${data.lastName}`}
-                            profession = {translate(data.profession, language)}
-                            experience = {`${translate(data.experience)} 15 ${translate("years")}`}
-                            statusStyle = "planned" />
-                }
-                
-                if(activeStatusLink === "status-past" && data.status.It_took_place) {
-                    return <AddedAppointments
-                            calendarImage = {calendarImg}
-                            appointmentDate = {data.date}
-                            clockImage = {clockImg}
-                            time = {`${data.time.hours}:${data.time.minutes}:${data.time.seconds}`}
-                            chatImage = {data.connectionType === "Video_connection" ? videoCameraImg : chatImg}
-                            connectionType = {translate(data.connectionType, language)}
-                            status = {translate("It_took_place", language)}
-                            docImage = {docImg}
-                            fullName = {`${data.firstName} ${data.lastName}`}
-                            profession = {translate(data.profession, language)}
-                            experience = {`${translate(data.experience)} 15 ${translate("years")}`}
-                            statusStyle = "it-took-place" />
-                }
-
-                if(activeStatusLink === "status-cancelled" && data.status.cancelled) {
-                    return <AddedAppointments 
-                        calendarImage = {calendarImg}
-                        appointmentDate = {data.date}
-                        clockImage = {clockImg}
-                        time = {`${data.time.hours}:${data.time.minutes}:${data.time.seconds}`}
-                        chatImage = {data.connectionType === "Video_connection" ? videoCameraImg : chatImg}
-                        connectionType = {translate(data.connectionType, language)}
-                        status = {translate("Cancelled", language)}
-                        docImage = {docImg}
-                        fullName = {`${data.firstName} ${data.lastName}`}
-                        profession = {translate(data.profession, language)}
-                        experience = {`${translate(data.experience)} 15 ${translate("years")}`}
-                        statusStyle = "cancelled" />
-                }
+            {filteredAppointments.map((data) => {
+                return <AddedAppointments key={data.id} appointmentData={data}/>  
             })}
         </section>
     );
