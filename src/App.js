3ad5497f449dc7d75093dc50307d5ea88baf4db2
varspijoyan <varsikpijoyan2003@gmail.com>
2025-02-11 17:54:49 +0400

    Added navigation to Doctor Page

--- src/App.js
+++ src/App.js
@@ -2,19 +2,20 @@ import "./App.css";
 import Header from "./components/header-component/Header";
 import LanguageSwitchProvider from "../src/contexts/LanguageSwitch";
 import MainPage from "./pages/MainPage";
-import { Navigate, Route, Routes } from "react-router-dom";
+import {Route, Routes } from "react-router-dom";
 import AppointmentList from "./components/appointment-list-component/AppointmentList";
 import Footer from "./components/footer-component/Footer";
+import DoctorProfilePage from "./pages/DoctorProfilePage";
 
 function App() {
   return (
     <LanguageSwitchProvider>
       <Header />
       <Routes>
-        <Route path="/" element={<Navigate to="appointments/upcoming" />}/>
-        <Route path="/appointments" element={<MainPage />}>
+        <Route path="/" element={<MainPage />}>
           <Route path=":status" element={<AppointmentList />}/>
         </Route>
+        <Route path="/doctor-profile/:docId" element={<DoctorProfilePage />} />
       </Routes>
       <Footer />
     </LanguageSwitchProvider>
--- src/components/appointment-list-component/ViewAppointmentButtonComponent.jsx
+++ src/components/appointment-list-component/ViewAppointmentButtonComponent.jsx
@@ -1,8 +1,15 @@
 import { useContext } from "react"
 import { LanguageSwitch } from "../../contexts/LanguageSwitch"
 import { translate } from "../../utils/translate";
+import { Link } from "react-router-dom";
 
-export default function ViewAppointmentButtonComponent() {
-    const {language} = useContext(LanguageSwitch);
-    return <button className="appointment-btn-1">{translate("View", language)} {translate("the_appointment", language)}</button>
+export default function ViewAppointmentButtonComponent({ doctorId }) {
+    const { language } = useContext(LanguageSwitch);
+    return (
+        <button className="appointment-btn-1">
+            <Link to={`/doctor-profile/${doctorId}`}>
+                {translate("View", language)} {translate("the_appointment", language)}
+            </Link>
+        </button>
+    );
 }
