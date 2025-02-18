616c90943d6b2d3e14d575adbc44bcf48e11dc37
varspijoyan <varsikpijoyan2003@gmail.com>
2025-02-18 20:44:27 +0400

    Fixed all the translation in the components inside appointment-section-component folder

--- src/components/appointment-section-component/appointment-text-container-component/AppointmentTextContainerComponent.jsx
+++ src/components/appointment-section-component/appointment-text-container-component/AppointmentTextContainerComponent.jsx
@@ -1,26 +1,28 @@
+import { useTranslation } from "react-i18next"
 
 export default function AppointmentTextContainerComponent() {
-    const {language} = useContext(LanguageSwitch);
+    const { t } = useTranslation();
+
     return (
         <div className="appointment-text-container">
-            <h1 className="appointment-section-heading">{translate("Make_an_appointment_with_a_doctor", language)}</h1>
+            <h1 className="appointment-section-heading">{t("makeAnAppointmentSection.heading")}</h1>
             <div className={`heading-border ${language === "ru" ? "ru-heading-border" : "en-heading-border"}`}></div>
-            <p className="appointment-section-desc">{translate("More_than", language)} 1500 {translate("doctors", language)} {translate("and", language)} {translate("more_than", language)} 50 {translate("different", language)} {translate("specializations", language)}</p>
+            <p className="appointment-section-desc">{t("makeAnAppointmentSection.description")}</p>
             <ul>
                 <li>
                     <div className="circle"></div>
-                    <p className="service">{translate("Reception_at_a_time_for_you", language)}</p>
+                    <p className="service">{t("makeAnAppointmentSection.service1")}</p>
                 </li>
                 <li>
                     <div className="circle"></div>
-                    <p className="service">{translate("Doctors_with_more_than_8_years_of_experience", language)}</p>
+                    <p className="service">{t("makeAnAppointmentSection.service2")}</p>
                 </li>
                 <li>
                     <div className="circle"></div>
-                    <p className="service">{translate("We_will_clearly_explain_the_cause_of_the_illness_and_give_detailed_recommendations", language)}</p>
+                    <p className="service">{t("makeAnAppointmentSection.service3")}</p>
                 </li>
             </ul>
-            <a href="#" className="link">{translate("Make_an_appointment", language)}</a>
+            <a href="#" className="link">{t("makeAnAppointmentSection.link")}</a>
         </div>
     )
 }
