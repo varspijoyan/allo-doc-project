df5e9b5f9dad22a7445b220e6667f1e3e74f51c7
varspijoyan <varsikpijoyan2003@gmail.com>
2025-02-18 18:01:32 +0400

    Added file for language translation using i18next library

---
+++ src/i18n.js
@@ -0,0 +1,19 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from 'i18next-browser-languagedetector';
+import en from "./locals/en.json";
+import ru from "./locals/ru.json";
+
+i18n
+.use(initReactI18next)
+.use(LanguageDetector)
+.init({
+    debugger: true,
+    fallbackLng: 'ru',
+    recources: {
+        ru,
+        en
+    }
+});
+
+export default i18n;
