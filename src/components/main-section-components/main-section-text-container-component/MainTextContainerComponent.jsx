d01d46de2b62ee2f4703a77a7d370acec3b43f7c
varspijoyan <varsikpijoyan2003@gmail.com>
2025-02-18 20:38:53 +0400

    Fixed all the translation in the components inside main-section-component folder

--- src/components/main-section-components/main-section-text-container-component/MainTextContainerComponent.jsx
+++ src/components/main-section-components/main-section-text-container-component/MainTextContainerComponent.jsx
@@ -1,13 +1,15 @@
-
 import MakeAnAppointmentButtonComponent from "../main-section-button-component/MakeAnAppointmentButtonComponent";
 import HowItWorksButtonComponent from "../main-section-button-component/HowItWorksButtonComponent";
+import { useTranslation } from "react-i18next";
+
 export default function MainTextContainerComponent() {
-    const {language} = useContext(LanguageSwitch);
+    const { t } = useTranslation();
+
     return (
         <div className="main-section-text-container">
-            <h1 className="heading">{translate("Online", language)} {translate("consultations", language)} {translate("from_doctors", language)} {translate("specialists", language)} 24/7</h1>
+            <h1 className="heading">{t("mainSection.heading")} 24/7</h1>
             <div className={`h1-border ${language === "ru" ? "ru-h1-border" : "en-h1-border"}`}></div>
-            <p className="desc">{translate("Consult", language)} <b>{translate("now", language)}</b> <span className={`b-1 ${language === "ru" ? "ru-b-1" : "en-b-1"}`}></span> {translate("or", language)} {translate("by", language)} <b>{translate("previous_appointment", language)}</b> <span className={`b-2 ${language === "ru" ? "ru-b-2" : "en-b-2"}`}></span> {translate("from_your", language)} {translate("computer", language)} {translate("or", language)} {translate("by_using_our_application", language)}, {translate("available_in", language)} <span className="highlight-blue">App Store</span> {translate("and", language)} <span className="highlight-blue">Google Play</span></p>
+            <p className="desc">{t("mainSection.descriptionPart1")} <b>{t("mainSection.higlightDescriptionWithBTeg1")}</b> <span className={`b-1 ${language === "ru" ? "ru-b-1" : "en-b-1"}`}></span> {t("mainSection.descriptionPart2")} {t("mainSection.descriptionPart3")} <b>{t("mainSection.highlightDescriptionWithBTeg4")}</b> <span className={`b-2 ${language === "ru" ? "ru-b-2" : "en-b-2"}`}></span> {t("mainSection.descriptionPart4")} {t("mainSection.descriptionPart2")} {t("mainSection.descriptionPart5")} <span className="highlight-blue">App Store</span> {t("mainSection.descriptionPart6")} <span className="highlight-blue">Google Play</span></p>
             <div className="buttons">
                 <MakeAnAppointmentButtonComponent />
                 <HowItWorksButtonComponent />
